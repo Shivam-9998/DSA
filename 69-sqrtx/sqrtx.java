34703ca82ebb78b239e5b0c97b391f32ab1d9c68
Shivam-9998 <shivamthakker9998@gmail.com>
2026-07-23 08:58:14 +0530

    Time: 1 ms (97.43%) | Memory: 42.4 MB (79.01%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        // int low=0;
+        // int high=x;
+        // int res=0;
+        // while(true)
+        // {
+        //     res=(low+high)/2
+        //     if(res*res==x)
+        //     return x
+        //     else if(res*res<x)
+        //     {
+
+        //     }
+        // }
+        int root=(int)Math.sqrt(x);
+        return root;
+    }
+}
